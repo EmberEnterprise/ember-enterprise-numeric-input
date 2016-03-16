8c928effc341a62acae2e279f7897360a9658f69
Anitsirc93 <cristina.dochitoiu1993@gmail.com>
2016-03-16 16:45:25 +0200

    added more tests WIP

--- tests/integration/components/e-numeric-input-test.js
+++ tests/integration/components/e-numeric-input-test.js
@@ -10,69 +10,125 @@ moduleForComponent('e-numeric-input', {
     integration: true,
 });
 
+function imitateClick(button, component) {
+    button.focus();
+    button.click();
+    component.blur();
+}
+
+// TODO: check why fillIn does not work! --> then some tests can be 'merged'
+// TODO: check why tests are failing if test window is not in focus when running them
+
 test('test up-down for simple input without restrictions', function(assert) {
     this.render(hbs`{{e-numeric-input value=5 }}`);
 
-    assert.equal(this.$('input').val(), '5', 'given value is displayed in input');
+    const inputElement = this.$('input');
+    const upButton = this.$('.e-numeric-up');
+    const downButton = this.$('.e-numeric-down');
+    const component = this.$('span');
 
-    this.$('.e-numeric-up').click();
-    assert.equal(this.$('input').val(), '6', 'value is correct after first increment');
+    assert.equal(inputElement.val(), '5', 'given value is displayed in input');
 
-    this.$('.e-numeric-up').click();
-    assert.equal(this.$('input').val(), '7', 'value is correct after second increment');
+    imitateClick(upButton, component);
+    assert.equal(inputElement.val(), '6', 'value is correct after first increment');
 
-    this.$('.e-numeric-down').click();
-    assert.equal(this.$('input').val(), '6', 'value is correct after first decrement');
+    imitateClick(upButton, component);
+    assert.equal(inputElement.val(), '7', 'value is correct after second increment');
+
+    imitateClick(downButton, component);
+    assert.equal(inputElement.val(), '6', 'value is correct after first decrement');
 });
 
 test('test down for simple input with min value restriction', function(assert) {
     this.render(hbs`{{e-numeric-input min=0 value=2 }}`);
 
-    this.$('.e-numeric-down').click();
-    assert.equal(this.$('input').val(), '1', 'value is correct after first decrement');
+    const inputElement = this.$('input');
+    const downButton = this.$('.e-numeric-down');
+    const component = this.$('span');
 
-    this.$('.e-numeric-down').click();
-    assert.equal(this.$('input').val(), '0', 'value is correct after second decrement');
+    imitateClick(downButton, component);
+    assert.equal(inputElement.val(), '1', 'value is correct after first decrement');
 
-    this.$('.e-numeric-down').click();
-    assert.equal(this.$('input').val(), '0', 'value hit min and did not change');
+    imitateClick(downButton, component);
+    assert.equal(inputElement.val(), '0', 'value is correct after second decrement');
+
+    imitateClick(downButton, component);
+    assert.equal(inputElement.val(), '0', 'value hit min and did not change');
 });
 
 test('test up for simple input with max value restriction', function(assert) {
     this.render(hbs`{{e-numeric-input max=100 value=98 }}`);
 
-    this.$('.e-numeric-up').click();
-    assert.equal(this.$('input').val(), '99', 'value is correct after first increment');
+    const inputElement = this.$('input');
+    const upButton = this.$('.e-numeric-up');
+    const component = this.$('span');
 
-    this.$('.e-numeric-up').click();
-    assert.equal(this.$('input').val(), '100', 'value is correct after second increment');
+    imitateClick(upButton, component);
+    assert.equal(inputElement.val(), '99', 'value is correct after first increment');
 
-    this.$('.e-numeric-up').click();
-    assert.equal(this.$('input').val(), '100', 'value hit max and did not change');
+    imitateClick(upButton, component);
+    assert.equal(inputElement.val(), '100', 'value is correct after second increment');
+
+    imitateClick(upButton, component);
+    assert.equal(inputElement.val(), '100', 'value hit max and did not change');
 });
 
 test('test if input value hits min', function(assert) {
     this.render(hbs`{{e-numeric-input min=0 step=3 value=5 }}`);
 
-    this.$('.e-numeric-down').click();
-    assert.equal(this.$('input').val(), '2', 'value is correct after first decrement');
+    const inputElement = this.$('input');
+    const downButton = this.$('.e-numeric-down');
+    const component = this.$('span');
 
-    this.$('.e-numeric-down').click();
-    assert.equal(this.$('input').val(), '0', 'value hit min');
+    imitateClick(downButton, component);
+    assert.equal(inputElement.val(), '2', 'value is correct after first decrement');
 
-    this.$('.e-numeric-down').click();
-    assert.equal(this.$('input').val(), '0', 'value still is min and did not change');
+    imitateClick(downButton, component);
+    assert.equal(inputElement.val(), '0', 'value hit min');
+
+    imitateClick(downButton, component);
+    assert.equal(inputElement.val(), '0', 'value still is min and did not change');
 });
 
 test('test if input value hits max', function(assert) {
     this.render(hbs`{{e-numeric-input max=100 step=3 value=95 }}`);
 
-    this.$('.e-numeric-up').click();
-    assert.equal(this.$('input').val(), '98', 'value is correct after first increment');
+    const inputElement = this.$('input');
+    const upButton = this.$('.e-numeric-up');
+    const component = this.$('span');
 
-    this.$('.e-numeric-up').click();
-    assert.equal(this.$('input').val(), '100', 'value hit max');
+    imitateClick(upButton, component);
+    assert.equal(inputElement.val(), '98', 'value is correct after first increment');
 
-    this.$('.e-numeric-up').click();
-    assert.equal(this.$('input').val(), '0', 'value still is max and did not change');
+    imitateClick(upButton, component);
+    assert.equal(inputElement.val(), '100', 'value hit max');
+
+    imitateClick(upButton, component);
+    assert.equal(inputElement.val(), '100', 'value still is max and did not change');
+});
+
+test('test up-down for currency input with restrictions', function(assert) {
+    this.render(hbs`{{e-numeric-input format="currency" max=6 min=4 value=5 }}`);
+
+    const inputElement = this.$('input');
+    const upButton = this.$('.e-numeric-up');
+    const downButton = this.$('.e-numeric-down');
+    const component = this.$('span');
+
+    assert.equal(inputElement.val(), '$5', 'given value is displayed in input');
+
+    imitateClick(upButton, component);
+    assert.equal(inputElement.val(), '$6', 'value is correct after first increment');
+
+    imitateClick(upButton, component);
+    assert.equal(inputElement.val(), '$6', 'value is still max after third increment');
+
+    imitateClick(downButton, component);
+    assert.equal(inputElement.val(), '$5', 'value is correct after first decrement');
+
+    imitateClick(downButton, component);
+    assert.equal(inputElement.val(), '$4', 'value is correct after second decrement');
+
+    imitateClick(downButton, component);
+    assert.equal(inputElement.val(), '$4', 'value is still min after third decrement');
 });
